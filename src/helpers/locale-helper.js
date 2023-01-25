10de08163aa296827e591f1b4a157f7ddbf004f9
Mehmet Emin Çatal <mhmtmnctl20@gmail.com>
2023-01-25 21:07:19 +0300

    redux

--- src/helpers/locale-helper.js
+++ src/helpers/locale-helper.js
@@ -1,6 +1,24 @@
+import store from "../store";
+import { setLang } from "../store/slices/locale-slice";
 
 export const langs = [
-    {title: "English", code: "en", country: "gb"},
-    {title: "Türkçe", code: "tr", country: "tr"},
-    {title: "Francais", code: "fr", country: "fr"},
-]
+  { title: "English", code: "en", country: "gb" },
+  { title: "Türkçe", code: "tr", country: "tr" },
+  { title: "Francais", code: "fr", country: "fr" },
+  { title: "Deutsch", code: "de", country: "de" },
+  { title: "Russia", code:"ru", country:"ru"}
+];
+
+export const getCurrentLang = () => {
+  return store.getState().locale.lang;
+};
+
+export const setCurrentLang = (lang) => {
+    store.dispatch(setLang(lang))
+};
+
+export const $t = (key) => {
+    const currentLang = getCurrentLang();
+    const langFile = require(`../helpers/langs/${currentLang.code}.json`);
+    return langFile[key];
+};
